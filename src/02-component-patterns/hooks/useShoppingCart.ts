7b8591802205666b3fd53065ae3d11a4568085b8
alejandro <dban095@hotmail.com>
2024-05-15 19:01:51 -0400

    control-props-alt

--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -6,32 +6,16 @@ export const useShoppingCart = () => {
     const [shoppingCart, setShoppingCart] = useState<{[key: string]: ProductInCart}>({})
 
     const onProductCountChange = ({ count, product }: {count: number, product: Product}) => {
-      setShoppingCart(oldShoppingCart => { 
-        const productinCart: ProductInCart = oldShoppingCart[product.id] || {
-          ...product,
-          count: 0
+      setShoppingCart(oldShoppingCart => {   
+        if ( count === 0) {
+          const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+          return rest;
         }
   
-        if( Math.max( productinCart.count + count, 0) > 0 ) {
-          productinCart.count += count
-          return {
-            ...oldShoppingCart,
-            [product.id]: productinCart
-          }
+        return {
+          ...oldShoppingCart,
+          [product.id]: {...product,count}
         }
-        //Borrar el Producto
-        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-        return rest;
-  
-        // if ( count === 0) {
-        //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-        //   return rest;
-        // }
-  
-        // return {
-        //   ...oldShoppingCart,
-        //   [product.id]: {...product,count}
-        // }
       })
     }
 
